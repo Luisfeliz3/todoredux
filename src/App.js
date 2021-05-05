f00681a5a12e811b821bbf9ee30d33df8e75a9f9
Luis Feliz <Luisfeliz@gmail.com>
2021-05-04 20:48:57 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+       <div clasName ="Todo-App">
+         <form>
+           <input type = "text"/>
+           
+         </form>
+
+         <div className="Todo-List">
+           <ul>
+             <li >
+               <input type = "checkbox"/> Create static UI
+             </li>
+             <li >
+               <input type = "checkbox"/> Create INitial State 
+             </li>
+             <li >
+               <input type = "checkbox"/> Use state to Render UI
+             </li>
+           </ul>
+         </div>
+
+       </div>
       </header>
     </div>
   );
